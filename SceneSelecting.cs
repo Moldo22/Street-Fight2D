c2c2477c0895e949970d7772e31c68c1bb984817
Moldovan Alex <115102799+Moldo22@users.noreply.github.com>
2022-12-06 15:17:33 +0200

    Update SceneSelecting.cs

--- SceneSelecting.cs
+++ SceneSelecting.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,10 @@ using UnityEngine.SceneManagement;
 public class SceneSelecting : MonoBehaviour
 {
     #region ScenesWindow
+    public static void Start()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
     public static void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
@@ -14,6 +19,22 @@ public class SceneSelecting : MonoBehaviour
     public static void PreviousLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1);
+    }
+
+    public static void Restart()
+    {
+        try
+        {
+          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-2);
+        }catch(Exception){};
+        try
+        {
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1);
+        }catch(Exception){};
+    }
+
+    public static void Quit() {
+        Application.Quit();
     }
     #endregion
 }
