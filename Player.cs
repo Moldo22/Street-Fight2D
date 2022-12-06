d94083da4c79ec4e58c43618a52e8aa7b975c828
Moldovan Alex <115102799+Moldo22@users.noreply.github.com>
2022-12-06 15:16:02 +0200

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -10,11 +10,13 @@ public class Player : MonoBehaviour
     [SerializeField] EnemyData enData1;
     [SerializeField] EnemyData enData2;
     [SerializeField] EnemyData playerData;
+    [SerializeField] GameObject RespawnBtn;
     private Rigidbody2D rb;
     private float movement;
     private float speed=2;
     private float jumpForce=7;
     public static bool onGround;
+    private bool enemyCollision=false;
     public static bool Dead=false;
     public static bool OtherLevel=true;
     public static int CurrentLevel=1;
@@ -50,10 +52,12 @@ public class Player : MonoBehaviour
     #region Collsion
     private void OnCollisionEnter2D(Collision2D col) {
         if (col.gameObject.tag=="Teren") onGround=true;
+        if (col.gameObject.tag=="Enemy") enemyCollision=true;
     }
 
     private void OnCollisionExit2D(Collision2D col) {
         if (col.gameObject.tag=="Teren") onGround=false;
+        if (col.gameObject.tag=="Enemy") enemyCollision=false;
     }
 #endregion
 
@@ -61,9 +65,10 @@ private void Die() {
         Destroy(gameObject);
 }   
 
-
     void Update()
     {
+        if (playerData.health<1) RespawnBtn.SetActive(true);
+        else RespawnBtn.SetActive(false);
         //Dead Animation Statement
         if (Dead) animator.SetBool("isDead",true);
         //
@@ -72,19 +77,16 @@ private void Die() {
         if (Input.GetKeyDown(KeyCode.Space)) animator.SetBool("Attack",true);
         else animator.SetBool("Attack",false);
         if (!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow)) movement=Mathf.Abs(Input.GetAxisRaw("Horizontal"));
-        if (onGround) animator.SetFloat("Move",movement);
+        if (onGround || enemyCollision) animator.SetFloat("Move",movement);
         //
-        if ((Input.GetKeyDown(KeyCode.W)) && onGround) 
-        {
-            rb.AddForce((Vector2.up*jumpForce),ForceMode2D.Impulse);
-            if (!onGround) animator.SetFloat("Move",0);
-        }
-        if (Input.GetKey(KeyCode.D) && onGround) 
+        if ((Input.GetKeyDown(KeyCode.W)) && onGround) rb.AddForce((Vector2.up*jumpForce),ForceMode2D.Impulse);
+        else if (enemyCollision && (Input.GetKeyDown(KeyCode.W))) rb.AddForce((Vector2.up*jumpForce),ForceMode2D.Impulse);
+        if (Input.GetKey(KeyCode.D) && (onGround || enemyCollision)) 
         {
             rb.velocity=transform.right*speed;
             transform.localScale=new Vector2(-3,3);
         }
-        if (Input.GetKey(KeyCode.A) && onGround) 
+        if (Input.GetKey(KeyCode.A) && (onGround || enemyCollision)) 
         {
             rb.velocity=transform.right*-1*speed;
             transform.localScale=new Vector2(3,3);
